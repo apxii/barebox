88cb5ce029891edfa67169801ce3a3f5fc4836ea
Lucas Stach <dev@lynxeye.de>
2015-03-01 14:17:00 +0100

    ARM: move virt<->phys translation to io.h
    That's the place where Linux has them and other architectures would
    implement this.
    
    This will help in phasing out the direct usage of the ARM asm/mmu.h
    header.
    
    Signed-off-by: Lucas Stach <dev@lynxeye.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/include/asm/mmu.h
+++ arch/arm/include/asm/mmu.h
@@ -39,8 +39,6 @@ void dma_free_coherent(void *mem, size_t size);
 void dma_clean_range(unsigned long, unsigned long);
 void dma_flush_range(unsigned long, unsigned long);
 void dma_inv_range(unsigned long, unsigned long);
-unsigned long virt_to_phys(volatile void *virt);
-void *phys_to_virt(unsigned long phys);
 void remap_range(void *_start, size_t size, uint32_t flags);
 void *map_io_sections(unsigned long physaddr, void *start, size_t size);
 uint32_t mmu_get_pte_cached_flags(void);
@@ -55,16 +53,6 @@ static inline void *dma_alloc_coherent(size_t size)
 static inline void dma_free_coherent(void *mem, size_t size)
 {
 	free(mem);
-}
-
-static inline void *phys_to_virt(unsigned long phys)
-{
-	return (void *)phys;
-}
-
-static inline unsigned long virt_to_phys(volatile void *mem)
-{
-	return (unsigned long)mem;
 }
 
 static inline void dma_clean_range(unsigned long s, unsigned long e)
